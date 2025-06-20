9af4cac130295bbe5d938c9b8e942febf9a747e6
Sidharth Niwas Sharma <snsharma@berkeley.edu>
2025-06-20 03:02:22 -0700

    some dummy changes

--- tools/dummy/src/dummytool.rs
+++ tools/dummy/src/dummytool.rs
@@ -1,6 +1,9 @@
 use shammer::flow::{FlowNode, Tool, Step};
-use std::fs;
+use std::char;
+use std::fs::{self, File};
 use std::sync::Arc;
+use std::io::prelude::*;
+
 use std::process::Command;
 use std::path::{Path, PathBuf};
 
@@ -22,31 +25,70 @@ impl Tool for DummyTool {
     }
 
     fn invoke(&self, steps:Vec<Step>) {
+        // let dummyDB_stat = Command::new("zsh")
+        //     .arg("-c")
+        //     .arg("echo \"start\" >> dummydb.txt")
+        //     .current_dir(&self.work_dir)
+        //     .status()
+        //     .expect("Failed");
+        // if !dummyDB_stat.success() {
+        //             eprintln!("Failed to write checkpoint to file");
+        // }
+
         for step in steps {
            println!("  - Running step: '{}'", step.name);
             println!("    Command: {}", step.command);
+        
+                let status = Command::new("zsh")
+                    .arg("-c")
+                    .arg(&step.command) 
+                    .current_dir(&self.work_dir) 
+                    .status() 
+                    .expect("Failed");    
 
-            let status = Command::new("bash")
-                .arg(&step.command) 
-                .current_dir(&self.work_dir) 
-                .status() 
-                .expect("Failed");
- 
+                if !status.success() {
+                    eprintln!("Error: Step '{}' failed with exit code {}", step.name, status);
+                    panic!("Stopping flow.");
+                }
 
-            if !status.success() {
-                eprintln!("Error: Step '{}' failed with exit code {}", step.name, status);
-                panic!("Stopping flow.");
-            }
+                if step.checkpoint {
 
-            if step.checkpoint {
-                self.write_checkpoint(&self.work_dir.join(format!("{}.checkpoint", step.name)));
-            }       
+                    let temp = self.write_checkpoint(&self.work_dir.join(format!("{}.checkpoint", step.name)));
+                    //let mut file = File::create_new("temp.sh").expect("failed to make temp file");
+                    //file.write_all(format!("!/bin/bash\n{}",&temp.command));
+
+                    let check_status = Command::new("zsh")
+                        .arg("-c")
+                        .arg(&temp.command)
+                        .current_dir(&self.work_dir)
+                        .status()
+                        .expect("Failed");
+                    if !check_status.success() {
+                        eprintln!("Failed to write checkpoint to file");
+                    }
+                }
+            
+
+            // if step.checkpoint {
+            //     let check_status = Command::new("bash")
+            //         .arg("-c")
+            //         .arg(format!("echo $dummydummyDB > {}.txt", step.name))
+            //         .current_dir(&self.work_dir)
+            //         .status()
+            //         .expect("failed");
+
+            //     if !check_status.success() {
+            //         eprintln!("Error: Checkpoint Step '{}' failed with exit code {}", step.name, check_status);
+            //         panic!("Stopping flow.");
+            //     }
+
+            // }     
         }
     }
 
     fn write_checkpoint(&self, path: &PathBuf) -> Step {
-       let checkpoint_command = format!("echo 'checkpoint data' > {}", path.to_str().unwrap());
-        println!("  - Writing checkpoint with command: {}", checkpoint_command);
+       let checkpoint_command = format!("cat dummydb.txt > {}", path.to_str().unwrap());
+        println!("  - Writing checkpoint w command: {}", checkpoint_command);
 
         Step {
             name: "write_checkpoint".to_string(),
@@ -66,9 +108,14 @@ impl Tool for DummyTool {
         }
     }
 
-    /// Checkpoint paths for each step.
     fn checkpoints(&self, steps: Vec<Step>) -> Vec<PathBuf> {
-        unimplemented!();
+        let mut ret : Vec<PathBuf> = vec![];
+        for step in steps.into_iter() {
+            if step.checkpoint {
+                ret.push(self.work_dir.join(format!("{}.checkpoint", step.name)))
+            }
+        }
+        return ret;
     }
 
     
@@ -91,28 +138,38 @@ mod tests {
     fn test_basic_flow() {
         
         let s1 = Step{
-            name: "step 1".to_string(),
-            command: "./src/dummyscript.sh".to_string(),
-            checkpoint: false,
+            name: "step_1".to_string(),
+            //command: "dummyDB=\"1\"".to_string(),
+            command : "echo \"1\" >> dummydb.txt".to_string(),
+            checkpoint: true,
+        };
+
+        let s15 = Step {
+            name : "step_15".to_string(),
+            command : "echo \"step_1.5\"".to_string(),
+            checkpoint : false,
         };
 
         let s2 = Step {
-            name : "step 2".to_string(),
-            command : "./src/dummyscript.sh".to_string(),
-            checkpoint: false,
+            name : "step_2".to_string(),
+            //command : "dummyDB=\"2\"".to_string(),
+            command : "echo \"2\" >> dummydb.txt".to_string(),
+            checkpoint: true,
         };
 
         let x = DummyTool::new(PathBuf::from("."));
 
         let flno = FlowNode {
             name : "test".to_string(),
-            steps : vec![s1, s2],
+            steps : vec![s1, s15, s2],
             deps : vec![],
             tool : Arc::new(x)
             
         };
 
         flno.tool.invoke(flno.steps);
+
+
 
         assert_eq!(2+2, 4)
 

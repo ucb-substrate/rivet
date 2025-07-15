0763d457241fb4495a24919b1132b2852bc246bd
CONnor9029 <connorlu@gmail.com>
2025-07-14 18:51:03 -0700

    feat: mmmc tcl script generation

--- tools/genus/src/lib.rs
+++ tools/genus/src/lib.rs
@@ -1,7 +1,9 @@
 use crate::fs::File;
 use rivet::flow::{Step, Tool};
 use std::fmt::Debug;
+use std::fmt::Write as FmtWrite;
 use std::io::prelude::*;
+use std::io::Write;
 use std::path::PathBuf;
 use std::process::Command;
 use std::{fs, io};
@@ -46,13 +48,10 @@ impl Genus {
         )?;
 
         for step in steps.into_iter() {
-            //if step name is in a hashset of helper functions run the helper function instead
-
             writeln!(tcl_file, "puts\"{}\"", step.command.to_string())?;
             writeln!(tcl_file, "{}", step.command)?;
             if (step.checkpoint) {
                 //generate tcl for checkpointing
-                self.write_checkpoint(path);
             }
         }
         writeln!(tcl_file, "puts \"{}\"", "quit")?;
@@ -61,69 +60,108 @@ impl Genus {
         Ok(())
     }
 
-    fn init_environment(rtl: &PathBuf, top_module: &String) -> Step {
-        let mut command_init = String::new();
+    fn generate_mmmc_script(path: &PathBuf) {
+        let mut mmmc_file = File::create(path);
 
-        //writeln!(&mut command_init, "hi {}", 1)?; this is example of what to do instead of push_str
+        //need to work on generated sdc files as well
+        writeln!(
+            mmmc_file,
+            "create_constraint_mode -name {name} {sdc_files_arg} {ilm_sdc_files_arg}",
+        );
+
+        //for corner in corners
+        //create innovus library sets
+        writeln!(
+            mmmc_file,
+            "create_library_set -name {name}_set -timing [list {list}]",
+        );
+        //create Innovus timing conditions
+        writeln!(
+            mmmc_file,
+            "create_timing_condition -name {name}_cond -library_sets [list {name}_set]",
+        );
+        //create Innovus rc corners from qrc tech files
+        writeln!(
+            mmmc_file,
+            "create_rc_corner -name {name}_rc -temperature {tempInCelsius} {qrc}",
+        );
+        //create innovus delay corner
+        writeln!(mmmc_file,"create_delay_corner -name {name}_delay -timing_condition {name}_cond -rc_corner {name}_rc" , );
+        //create the analysis views
+        writeln!(mmmc_file,"create_analysis_view -name {name}_view -delay_corner {name}_delay -constraint_mode {constraint}" , );
+
+        writeln!(mmmc_file,"set_analysis_view -setup {{ {setup_views} }} -hold {{ {hold_views} {extra_views} }} {power}" , );
+    }
+
+    fn init_environment(&self, rtl: &PathBuf, top_module: &String) -> Step {
+        let mut command = String::new();
+
+        //writeln!(&mut command, "hi {}", 1)?; this is example of what to do instead of push_str
         // --- Clock Gating Setup ---
         // Corresponds to the "synthesis.clock_gating_mode" == "auto" check
-        command_init.push_str("set_db lp_clock_gating_infer_enable  true\n");
-        command_init.push_str("set_db lp_clock_gating_prefix {CLKGATE}\n");
-        command_init.push_str("set_db lp_insert_clock_gating  true\n");
-        command_init.push_str("set_db lp_clock_gating_register_aware true\n");
+
+        writeln!(&mut command, "set_db lp_clock_gating_infer_enable  true");
+        writeln!(&mut command, "set_db lp_clock_gating_prefix {CLKGATE}");
+        writeln!(&mut command, "set_db lp_insert_clock_gating  true");
+        writeln!(&mut command, "set_db lp_clock_gating_register_aware true");
 
         // --- MMMC and Library Setup ---
         // This path is hardcoded for now, but you would generate and write this file at runtime
-        command_init.push_str(
-            "read_mmmc /scratch/cs199-cbc/labs/sp25-chipyard/vlsi/build/lab4/syn-rundir/mmmc.tcl\n",
-        );
+
+        //make a mmmc.tcl file in the work directory ie workdir/mmmc.tcl
+        let mmmc_path = self.work_dir.join("mmmc.tcl");
+
+        //then we call the mmmc script that writes tcl to the file in the provided file path
+        Genus::generate_mmmc_script(&mmmc_path);
+
+        writeln!(&mut command, "read_mmmc {}", mmmc_path.display());
 
         //need to hardcode the lef file path
         // In a real implementation, you would need to get the LEF files from your technology configuration
-        command_init.push_str("read_physical -lef {lef_files}\n");
+        writeln!(&mut command, "read_physical -lef {}", lef.display());
 
         //this command is ignored for our decoder
         // In a real implementation, you would need to get the QRC tech files from your technology configuration
-        //command_init.push_str("set_db qrc_tech_file {qrc_tech_files}\n");
+        //writeln!("set_db qrc_tech_file {qrc_tech_files}");
 
         // --- HDL Input and Elaboration ---
         // In a real implementation, you would get the list of RTL files from your configuration
         // need to accept a parameter of the file path of our verilog
         // The rtl file path will be a parameter so we need to add a pathbuff
-        command_init.push_str("read_hdl -sv {rtl_files}\n");
+        writeln!(&mut command, "read_hdl -sv {}", rtl.display());
 
         //top module needs to be assigned to the name of our trl file so it is supposed to be
         //"decoder"
-        command_init.push_str("elaborate {top_module}\n");
-        command_init.push_str("init_design -top {top_module}\n");
+        writeln!(&mut command, "elaborate {top_module}");
+        writeln!(&mut command, "init_design -top {top_module}");
 
         // --- Constraints and Design Settings ---
-        command_init.push_str("report_timing -lint -verbose\n");
+        writeln!(&mut command, "report_timing -lint -verbose");
         // In a real implementation, you would read a UPF or CPF file!()
         // find what are the power_commands in hammer
-        //command_init.push_str("read_upf {power_intent_file}\n");
-        //command_init.push_str("apply_power_intent -summary\n");
+        //writeln!("read_upf {power_intent_file}");
+        //writeln!("apply_power_intent -summary");
         //
         //
         // Need to create a function that makes the powerspec
-        command_init.push_str("read_power_intent -cpf /scratch/cs199-cbc/labs/sp25-chipyard/vlsi/build/lab4/syn-rundir/power_spec.cpf\n");
+        writeln!(&mut command, "read_power_intent -cpf /scratch/cs199-cbc/labs/sp25-chipyard/vlsi/build/lab4/syn-rundir/power_spec.cpf");
 
-        command_init.push_str("apply_power_intent -summary\n");
-        command_init.push_str("commit_power_intent\n");
+        writeln!(&mut command, "apply_power_intent -summary");
+        writeln!(&mut command, "commit_power_intent");
 
-        command_init.push_str("set_db root: .auto_ungroup none\n");
+        writeln!(&mut command, "set_db root: .auto_ungroup none");
         // This setting would come from your configuration and are not necessary for the decoder
         //
-        //command_init.push_str("set_db phys_flow_effort high\n");
-        //command_init.push_str("set_db opt_spatial_effort extreme\n");
+        //writeln!("set_db phys_flow_effort high");
+        //writeln!("set_db opt_spatial_effort extreme");
 
         // --- "Don't Use" Cells ---
         // In a real implementation, you would generate a list of "don't use" cells
-        //command_init.push_str("set_dont_use {dont_use_cells}\n");
+        //writeln!("set_dont_use {dont_use_cells}");
 
         Step {
             name: "init_environment".to_string(),
-            command: command_init,
+            command: command,
             checkpoint: true,
         }
     }
@@ -133,14 +171,17 @@ impl Genus {
         // In a real implementation, this would be based on a setting like
         // `synthesis.genus.phys_flow_effort`. This example assumes "high" effort.
 
-        command.push_str("set_db invs_temp_dir temp_invs\n");
+        writeln!(&mut command, "set_db invs_temp_dir temp_invs");
         // The innovus binary path would be a configurable parameter.
-        command.push_str("set_db innovus_executable {innovus_bin_path}\n");
-        command.push_str("set_db predict_floorplan_enable_during_generic true\n");
-        command.push_str("set_db physical_force_predict_floorplan true\n");
-        command.push_str("set_db predict_floorplan_use_innovus true\n");
+        writeln!(&mut command, "set_db innovus_executable {innovus_bin_path}");
+        writeln!(
+            &mut command,
+            "set_db predict_floorplan_enable_during_generic true"
+        );
+        writeln!(&mut command, "set_db physical_force_predict_floorplan true");
+        writeln!(&mut command, "set_db predict_floorplan_use_innovus true");
 
-        command.push_str("predict_floorplan\n");
+        writeln!(&mut command, "predict_floorplan");
 
         Step {
             name: "predict_floorplan".to_string(),
@@ -154,7 +195,7 @@ impl Genus {
 
         // Based on `synthesis.genus.phys_flow_effort`.
         // if synthesis.genus.phys_flow_effort.lower() == "none"
-        command.push_str("syn_generic\n");
+        writeln!(&mut command, "syn_generic");
 
         //else append "syn_generic -physical"
 
@@ -167,10 +208,10 @@ impl Genus {
 
     fn syn_map() -> Step {
         let mut command = String::new();
-        command.push_str("syn_map\n");
+        writeln!(&mut command, "syn_map");
 
         // This corresponds to `synthesis.genus.phys_flow_effort` = "high"
-        //command.push_str("syn_opt -spatial\n");
+        //writeln!("syn_opt -spatial");
 
         Step {
             name: "syn_map".to_string(),
@@ -182,8 +223,8 @@ impl Genus {
     fn add_tieoffs() -> Step {
         let mut command = String::new();
 
-        command.push_str("set_db message:WSDF-201 .max_print 20\n");
-        command.push_str("set_db use_tiehilo_for_const duplicate\n");
+        writeln!(&mut command, "set_db message:WSDF-201 .max_print 20");
+        writeln!(&mut command, "set_db use_tiehilo_for_const duplicate");
 
         // The cell names {TIE_HI_CELL} and {TIE_LO_CELL} would be dynamically
         // retrieved from the technology configuration.
@@ -203,13 +244,17 @@ impl Genus {
         //
         //
         if true {
-            command.push_str("set ACTIVE_SET [string map { .setup_view .setup_set .hold_view .hold_set .extra_view .extra_set } [get_db [get_analysis_views] .name]]");
-            command.push_str("set HI_TIEOFF [get_db base_cell:TIEHI .lib_cells -if { .library.library_set.name == $ACTIVE_SET }]");
-            command.push_str("set LO_TIEOFF [get_db base_cell:TIELO .lib_cells -if { .library.library_set.name == $ACTIVE_SET }]");
-            command.push_str("add_tieoffs -high $HI_TIEOFF -low $LO_TIEOFF -max_fanout 1 -verbose");
+            writeln!(&mut command, "set ACTIVE_SET [string map { .setup_view .setup_set .hold_view .hold_set .extra_view .extra_set } [get_db [get_analysis_views] .name]]");
+            writeln!(&mut command, "set HI_TIEOFF [get_db base_cell:TIEHI .lib_cells -if { .library.library_set.name == $ACTIVE_SET }]");
+            writeln!(&mut command, "set LO_TIEOFF [get_db base_cell:TIELO .lib_cells -if { .library.library_set.name == $ACTIVE_SET }]");
+            writeln!(
+                &mut command,
+                "add_tieoffs -high $HI_TIEOFF -low $LO_TIEOFF -max_fanout 1 -verbose"
+            );
         } else {
-            command.push_str(
-                "add_tieoffs -high {TIE_HI_CELL} -low {LO_LO_CELL} -max_fanout 1 -verbose\n",
+            writeln!(
+                &mut command,
+                "add_tieoffs -high {TIE_HI_CELL} -low {LO_LO_CELL} -max_fanout 1 -verbose",
             );
         }
 
@@ -225,11 +270,11 @@ impl Genus {
         // This part of the command would be dynamically generated by helper
         // functions like `child_modules_tcl()` and `write_regs_tcl()` in the
         // original Python code to find and format register information.
-        command.push_str("set regs [find / -seq_cells]\n");
-        command.push_str("set reg_paths [get_db $regs .name]\n");
-        command.push_str("set fp [open \"find_regs_paths.json\" \"w\"]\n");
-        command.push_str("puts $fp $reg_paths\n");
-        command.push_str("close $fp\n");
+        writeln!(&mut command, "set regs [find / -seq_cells]");
+        writeln!(&mut command, "set reg_paths [get_db $regs .name]");
+        writeln!(&mut command, "set fp [open \"find_regs_paths.json\" \"w\"]");
+        writeln!(&mut command, "puts $fp $reg_paths");
+        writeln!(&mut command, "close $fp");
 
         Step {
             name: "write_regs".to_string(),
@@ -239,10 +284,11 @@ impl Genus {
     }
     fn generate_reports() -> Step {
         let mut command = String::new();
-        command.push_str("write_reports -directory reports -tag final\n");
-        //command.push_str("report_ple > reports/final_ple.rpt\n");
-        command.push_str(
-            "report_timing -unconstrained -max_paths 50 > reports/final_unconstrained.rpt\n",
+        writeln!(&mut command, "write_reports -directory reports -tag final");
+        //writeln!("report_ple > reports/final_ple.rpt");
+        writeln!(
+            &mut command,
+            "report_timing -unconstrained -max_paths 50 > reports/final_unconstrained.rpt",
         );
 
         Step {
@@ -256,28 +302,31 @@ impl Genus {
         let mut command = String::new();
 
         // The filenames would use a variable for the top module name.
-        //command.push_str("write_hdl > {top_module}.mapped.v\n");
-        command.push_str("write_hdl > /scratch/cs199-cbc/labs/sp25-chipyard/vlsi/build/lab4/syn-rundir/decoder.mapped.v");
+        //writeln!("write_hdl > {top_module}.mapped.v");
+        writeln!(&mut command, "write_hdl > /scratch/cs199-cbc/labs/sp25-chipyard/vlsi/build/lab4/syn-rundir/decoder.mapped.v");
 
-        //command.push_str("write_hdl -exclude_ilm > {top_module}_noilm.mapped.v\n");
-        //command.push_str("write_sdc -view {setup_view_name} > {top_module}.mapped.sdc\n");
-        //command.push_str("write_sdf > {top_module}.mapped.sdf\n");
+        //writeln!("write_hdl -exclude_ilm > {top_module}_noilm.mapped.v");
+        //writeln!("write_sdc -view {setup_view_name} > {top_module}.mapped.sdc");
+        //writeln!("write_sdf > {top_module}.mapped.sdf");
         //
         //// Corresponds to `phys_flow_effort` != "none"
-        //command.push_str("write_db -common\n");
+        //writeln!("write_db -common");
         //
         // change this tcl from hardcoded
 
         // verbose_append("write_template -full -outfile {}.mapped.scr".format(top))
-        command.push_str("write_template -full -outfile decoder.mapped.scr");
+        writeln!(
+            &mut command,
+            "write_template -full -outfile decoder.mapped.scr"
+        );
 
         //verbose_append("write_sdc -view {view} > {file}".format(view=view_name, file=self.mapped_sdc_path))
-        command.push_str("write_sdc -view ss_100C_1v60.setup_view > /scratch/cs199-cbc/labs/sp25-chipyard/vlsi/build/lab4/syn-rundir/decoder.mapped.sdc");
+        writeln!(&mut command, "write_sdc -view ss_100C_1v60.setup_view > /scratch/cs199-cbc/labs/sp25-chipyard/vlsi/build/lab4/syn-rundir/decoder.mapped.sdc");
 
         //verbose_append("write_sdf > {run_dir}/{top}.mapped.sdf".format(run_dir=self.run_dir, top=top))
-        command.push_str("write_sdf > /scratch/cs199-cbc/labs/sp25-chipyard/vlsi/build/lab4/syn-rundir/decoder.mapped.sdf");
+        writeln!(&mut command, "write_sdf > /scratch/cs199-cbc/labs/sp25-chipyard/vlsi/build/lab4/syn-rundir/decoder.mapped.sdf");
         //verbose_append("write_design -gzip_files {top}".format(top=top))
-        command.push_str("write_design -gzip_files decoder");
+        writeln!(&mut command, "write_design -gzip_files decoder");
 
         Step {
             name: "write_outputs".to_string(),
@@ -292,9 +341,9 @@ impl Genus {
 }
 
 impl Tool for Genus {
-    fn work_dir(&self) -> PathBuf {
-        self.work_dir.clone()
-    }
+    //fn work_dir(&self) -> PathBuf {
+    //    self.work_dir.clone()
+    //}
     // genus -files syn.tcl -no_gui
     fn invoke(&self, steps: Vec<Step>) {
         let mut tcl_path = PathBuf::new();
@@ -315,13 +364,13 @@ impl Tool for Genus {
         }
     }
 
-    fn checkpoint(&self, step: Step) -> PathBuf {
-        let mut ret = PathBuf::new();
-        ret.push(self.work_dir);
-        ret.push(format!("{}.checkpoint", step.name));
-
-        return ret;
-    }
+    //fn checkpoint(&self, step: Step) -> PathBuf {
+    //    let mut ret = PathBuf::new();
+    //    ret.push(self.work_dir);
+    //    ret.push(format!("{}.checkpoint", step.name));
+    //
+    //    return ret;
+    //}
 
     // fn write_checkpoint(&self, path: &PathBuf) -> Step {
     //     let checkpoint_command = format!("write_db -to_file {}", path.to_str().unwrap());

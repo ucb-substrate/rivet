0f7b649f4c575552e718cf8d7aa5509fc59a31be
CONnor9029 <connorlu@gmail.com>
2025-07-21 16:45:06 -0700

    fix: hardcoded file paths impl: power_intent

--- pdks/sky130/src/lib.rs
+++ pdks/sky130/src/lib.rs
@@ -9,7 +9,7 @@ use genus::Genus;
 use indoc::formatdoc;
 use rivet::flow::{Flow, FlowNode, Step};
 
-fn set_default_options() -> Step {
+pub fn set_default_options() -> Step {
     Step {
         name: "set_default_options".into(),
         command: r#"set_db hdl_error_on_blackbox true
@@ -95,7 +95,9 @@ pub fn read_design_files() -> Step {
     Step {
         checkpoint: true,
         command: r#"
-
+            {mmmc_tcl}
+            read_physical -lef { /scratch/cs199-cbc/labs/sp25-chipyard/vlsi/build/lab4/tech-sky130-cache/sky130_scl_9T.tlef /home/ff/eecs251b/sky130/sky130_cds/sky130_scl_9T_0.0.5/lef/sky130_scl_9T.lef }
+            read_hdl -sv { /scratch/cs199-cbc/labs/sp25-chipyard/vlsi/lab4/decoder.v }
 
             "#
         .into(),
@@ -121,7 +123,46 @@ pub fn init_design(module: &str) -> Step {
 
 pub fn power_intent() -> Step {
     // Write power_spec.cpf and run power_intent TCL commands.
-    todo!()
+    let mut power_spec_file = File::create("power_spec.cpf");
+    let power_spec_file_path = writeln!(
+        power_spec_file,
+        formatdoc! {
+        r#"
+    set_cpf_version 1.0e
+    set_hierarchy_separator /
+    set_design decoder
+    create_power_nets -nets VDD -voltage 1.8
+    create_power_nets -nets VPWR -voltage 1.8
+    create_power_nets -nets VPB -voltage 1.8
+    create_power_nets -nets vdd -voltage 1.8
+    create_ground_nets -nets { VSS VGND VNB vss }
+    create_power_domain -name AO -default
+    update_power_domain -name AO -primary_power_net VDD -primary_ground_net VSS
+    create_global_connection -domain AO -net VDD -pins [list VDD]
+    create_global_connection -domain AO -net VPWR -pins [list VPWR]
+    create_global_connection -domain AO -net VPB -pins [list VPB]
+    create_global_connection -domain AO -net vdd -pins [list vdd]
+    create_global_connection -domain AO -net VSS -pins [list VSS]
+    create_global_connection -domain AO -net VGND -pins [list VGND]
+    create_global_connection -domain AO -net VNB -pins [list VNB]
+    create_nominal_condition -name nominal -voltage 1.8
+    create_power_mode -name aon -default -domain_conditions {AO@nominal}
+    end_design
+    "#
+
+        }
+    );
+    //create the power_spec cpf file with the contents hard coded
+    Step {
+        checkpoint: true,
+        command: r#"
+            read_power_intent -cpf {power_spec_file_path}
+            apply_power_intent -summary
+            Commit_power_intent
+        "#
+        .into(),
+        name: "power_intent".into(),
+    }
 }
 
 pub fn syn_generic() -> Step {
@@ -154,10 +195,70 @@ pub fn add_tieoffs() -> Step {
     }
 }
 
-pub fn write_design() -> Step {
+pub fn write_design(module: &str) -> Step {
     // All write TCL commands
     // this includes write regs, write reports, write outputs
-    todo!()
+    Step {
+        checkpoint: true,
+        command: r#"
+        set write_cells_ir "./find_regs_cells.json"
+        set write_cells_ir [open $write_cells_ir "w"]
+        puts $write_cells_ir "\["
+
+        set refs [get_db [get_db lib_cells -if .is_sequential==true] .base_name]
+
+        set len [llength $refs]
+
+        for {set i 0} {$i < [llength $refs]} {incr i} {
+            if {$i == $len - 1} {
+                puts $write_cells_ir "    \"[lindex $refs $i]\""
+            } else {
+                puts $write_cells_ir "    \"[lindex $refs $i]\","
+            }
+        }
+
+        puts $write_cells_ir "\]"
+        close $write_cells_ir
+        set write_regs_ir "./find_regs_paths.json"
+        set write_regs_ir [open $write_regs_ir "w"]
+        puts $write_regs_ir "\["
+
+        set regs [get_db [get_db [all_registers -edge_triggered -output_pins] -if .direction==out] .name]
+
+        set len [llength $regs]
+
+        for {set i 0} {$i < [llength $regs]} {incr i} {
+            #regsub -all {/} [lindex $regs $i] . myreg
+            set myreg [lindex $regs $i]
+            if {$i == $len - 1} {
+                puts $write_regs_ir "    \"$myreg\""
+            } else {
+                puts $write_regs_ir "    \"$myreg\","
+            }
+        }
+
+        puts $write_regs_ir "\]"
+
+        close $write_regs_ir
+    puts "write_reports -directory reports -tag final" 
+    write_reports -directory reports -tag final
+    puts "report_timing -unconstrained -max_paths 50 > reports/final_unconstrained.rpt" 
+    report_timing -unconstrained -max_paths 50 > reports/final_unconstrained.rpt
+
+    puts "write_hdl > {module}.mapped.v" 
+    write_hdl > {module}.mapped.v
+    puts "write_template -full -outfile {module}.mapped.scr" 
+    write_template -full -outfile {module}.mapped.scr
+    puts "write_sdc -view ss_100C_1v60.setup_view > {module}.mapped.sdc" 
+    write_sdc -view ss_100C_1v60.setup_view > {module}.mapped.sdc
+    puts "write_sdf > {module}.mapped.sdf" 
+    write_sdf > {module}.mapped.sdf
+    puts "write_design -gzip_files {module}" 
+    write_design -gzip_files {module}
+            "#.into(),
+            //the paths for write hdl, write sdc, and write sdf need to be fixed
+        name: "write_design".into(),
+    }
 }
 
 pub fn genus_syn() -> FlowNode {}

9f838ff753c9a481e2e9f388e71666c33042470f
CONnor9029 <connorlu@berkeley.edu>
2026-01-26 10:28:00 -0800

    fix: BashStep api

--- rivet/src/bash.rs
+++ rivet/src/bash.rs
@@ -1,6 +1,6 @@
 use crate::Step;
 use std::fmt::Debug;
-use std::fs;
+use std::fs::File;
 use std::path::PathBuf;
 use std::process::Command;
 use std::sync::Arc;
@@ -8,21 +8,25 @@ use std::sync::Arc;
 #[derive(Debug)]
 pub struct BashStep {
     pub work_dir: PathBuf,
-    pub file_name: String,
+    pub name: String,
+    pub block: String,
     pub dependencies: Vec<Arc<dyn Step>>,
 }
 
 impl BashStep {
     pub fn new(
         work_dir: impl Into<PathBuf>,
-        file_name: impl Into<String>,
+        name: impl Into<String>,
+        block: impl Into<String>,
         deps: Vec<Arc<dyn Step>>,
     ) -> Self {
         let dir = work_dir.into();
-        let file = file_name.into();
+        let file = name.into();
+        let module = block.into();
         BashStep {
             work_dir: dir,
-            file_name: file,
+            name: file,
+            block: module,
             dependencies: deps,
         }
     }
@@ -30,9 +34,21 @@ impl BashStep {
 
 impl Step for BashStep {
     fn execute(&self) {
-        let status = Command::new("bash")
-            .args([format!("{}.sh", self.file_name)])
-            .current_dir(self.work_dir.clone())
+        let out_path = self
+            .work_dir
+            .join(format!("{}.{}.out", self.block, self.name));
+        let err_path = self
+            .work_dir
+            .join(format!("{}.{}.err", self.block, self.name));
+
+        let out_file = File::create(out_path).expect("Failed to create stdout file");
+        let err_file = File::create(err_path).expect("Failed to create stderr file");
+
+        let status = Command::new("/bin/bash")
+            .args([format!("run_{}.sh", self.name)])
+            .current_dir(&self.work_dir)
+            .stdout(out_file)
+            .stderr(err_file)
             .status()
             .expect("Failed to execute BashStep");
 

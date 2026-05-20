35c71e3c47e5ef6c786bc0394e8b4296dee12b0a
Rohan Kumar <rohankumar@berkeley.edu>
2026-05-20 12:08:01 -0700

    add RustStep

---
+++ rivet/src/rust.rs
@@ -0,0 +1,53 @@
+use crate::Step;
+use std::fmt::Debug;
+use std::path::PathBuf;
+use std::sync::Arc;
+
+#[derive(Clone)]
+pub struct RustStep<F> {
+    pub work_dir: PathBuf,
+    pub rs_fn: F,
+    pub dependencies: Vec<Arc<dyn Step>>,
+    pub pinned: bool,
+}
+
+impl<F> Debug for RustStep<F> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("RustStep")
+            .field("work_dir", &self.work_dir)
+            .field("rs_fn", &"<fn>")
+            .field("dependencies", &self.dependencies)
+            .field("pinned", &self.pinned)
+            .finish()
+    }
+}
+
+impl<F> RustStep<F> {
+    pub fn new(work_dir: impl Into<PathBuf>, rs_fn: F, deps: Vec<Arc<dyn Step>>) -> Self {
+        let dir = work_dir.into();
+        RustStep {
+            work_dir: dir,
+            rs_fn,
+            dependencies: deps,
+            pinned: false,
+        }
+    }
+
+    pub fn pin(&mut self) {
+        self.pinned = true;
+    }
+}
+
+impl<F: Fn(&PathBuf) + Send + Sync> Step for RustStep<F> {
+    fn execute(&self) {
+        (self.rs_fn)(&self.work_dir);
+    }
+
+    fn deps(&self) -> Vec<Arc<dyn Step>> {
+        self.dependencies.clone()
+    }
+
+    fn pinned(&self) -> bool {
+        self.pinned
+    }
+}

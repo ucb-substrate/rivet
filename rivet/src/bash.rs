1195a80e45df1de3d10e7086dafdb266cd6457f6
Rohan Kumar <rohankumar@berkeley.edu>
2026-05-21 16:26:54 -0700

    print bashstep output to command line

--- rivet/src/bash.rs
+++ rivet/src/bash.rs
@@ -1,9 +1,11 @@
 use crate::Step;
 use std::fmt::Debug;
 use std::fs::File;
+use std::io::{BufRead, BufReader, Write};
 use std::path::PathBuf;
-use std::process::Command;
+use std::process::{Command, Stdio};
 use std::sync::Arc;
+use std::thread;
 
 #[derive(Debug, Clone)]
 pub struct BashStep {
@@ -47,18 +49,41 @@ impl Step for BashStep {
             .work_dir
             .join(format!("{}.{}.err", self.block, self.name));
 
-        let out_file = File::create(out_path).expect("Failed to create stdout file");
-        let err_file = File::create(err_path).expect("Failed to create stderr file");
-
         // TODO: Make this similar to a TCL tool where a bash script is composed of several
         // substeps and templated here, rather than running a hardcoded script path.
-        let status = Command::new("/bin/bash")
+        let mut child = Command::new("/bin/bash")
             .args([format!("run_{}.sh", self.name)])
             .current_dir(&self.work_dir)
-            .stdout(out_file)
-            .stderr(err_file)
-            .status()
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .spawn()
             .expect("Failed to execute BashStep");
+
+        let stdout = BufReader::new(child.stdout.take().unwrap());
+        let stderr = BufReader::new(child.stderr.take().unwrap());
+
+        let stdout_thread = thread::spawn(move || {
+            let mut file = File::create(out_path).expect("Failed to create stdout file");
+            for line in stdout.lines() {
+                let line = line.unwrap();
+                println!("{}", line);
+                writeln!(file, "{}", line).unwrap();
+            }
+        });
+
+        let stderr_thread = thread::spawn(move || {
+            let mut file = File::create(err_path).expect("Failed to create stderr file");
+            for line in stderr.lines() {
+                let line = line.unwrap();
+                eprintln!("{}", line);
+                writeln!(file, "{}", line).unwrap();
+            }
+        });
+
+        stdout_thread.join().unwrap();
+        stderr_thread.join().unwrap();
+
+        let status = child.wait().expect("Failed to wait on BashStep");
 
         if !status.success() {
             panic!(
